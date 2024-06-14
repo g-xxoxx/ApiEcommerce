a93e82326b3f44b259ce2b96271a0720854b5f33
g-xxoxx <trungduc05112000@gmail.com>
2024-06-14 22:54:29 +0700

    connect-mongoose

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(5000, () => {
+mongoose.connect(
+    process.env.MONGO_URL
+)
+    .then(() => console.log(`DB Connection Successfull with ${process.env.PORT}!`))
+    .catch((err) => {
+        console.log("Err");
+    });
+
+app.listen(process.env.PORT || 8081, () => {
     console.log("Backend server is running...")
 })
